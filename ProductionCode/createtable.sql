731bcc1d4701d0be0bf1078076dd4eb3b42d446b
Gray Driscoll <driscollg2@carleton.edu>
2026-05-08 23:10:10 -0500

    Check tables

--- ProductionCode/createtable.sql
+++ ProductionCode/createtable.sql
@@ -1,10 +1,24 @@
-DROP TABLE IF EXISTS education_data
+--Literacy rate table
+DROP TABLE IF EXISTS education_data;
+
 CREATE TABLE education_data (
-  entity TEXT
-  code  CHAR[3]
-  year INTEGER
-  literacy_rate NUMMERIC
-  avg_years_of_education NUMERIC
-  population BIGINT
-  region TEXT
+    entity TEXT,
+    code CHAR(3),
+    year INTEGER,
+    literacy_rate NUMERIC,
+    avg_years_education NUMERIC,
+    population BIGINT,
+    region TEXT
+);
+
+
+--Women in gov table
+DROP TABLE IF EXISTS women_in_gov_data;
+
+CREATE TABLE women_gov_data (
+    entity TEXT,
+    code CHAR(3),
+    year INTEGER,
+    women_gov_percentage NUMERIC,
+    region TEXT
 );
